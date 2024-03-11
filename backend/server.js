b9155ff8ebf424431f813fd0c4b42a62de92b30f
Mohamad-Kazem Abd-el-Sater <mkhas313@gmail.com>
2024-03-11 10:04:16 +0200

    Test basic get route

--- backend/server.js
+++ backend/server.js
@@ -4,4 +4,12 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+/**
+ *
+ * GET - All Posts
+ */
+app.get("/api/posts", (req, res) => {
+	res.status(200).json({ message: "All Posts" });
+});
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
