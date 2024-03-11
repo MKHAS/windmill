0071f2c9554f4c30da17989e712b931a7ea29238
Mohamad-Kazem Abd-el-Sater <mkhas313@gmail.com>
2024-03-11 18:26:42 +0200

    Add the rest of the basic controller functions and configure postRoutes to use them

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,7 +1,50 @@
+/**
+ *
+ * @desc get Posts
+ * @route GET /api/posts
+ * @access Private
+ */
+
 const getPosts = (req, res) => {
 	res.status(200).json({ message: "Get All Posts" });
 };
 
+/**
+ *
+ * @desc Add a post
+ * @route Post /api/posts
+ * @access Private
+ */
+
+const addPost = (req, res) => {
+	res.status(200).json({ message: "Add a post" });
+};
+
+/**
+ *
+ * @desc Update a Post
+ * @route PUT /api/posts/:id
+ * @access Private
+ */
+
+const updatePost = (req, res) => {
+	res.status(200).json({ message: `Update Post with id : ${req.params.id}` });
+};
+
+/**
+ *
+ * @desc Delete a Post
+ * @route DELETE /api/posts/:id
+ * @access Private
+ */
+
+const deletePost = (req, res) => {
+	res.status(200).json({ message: `Delete Post with id : ${req.params.id}` });
+};
+
 module.exports = {
 	getPosts,
+	addPost,
+	updatePost,
+	deletePost,
 };
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getPosts } = require("../controllers/postController");
+const {
+	getPosts,
+	addPost,
+	updatePost,
+	deletePost,
+} = require("../controllers/postController");
 
 /**
  *
@@ -12,24 +17,18 @@ router.get("/", getPosts);
  *
  * POST - Add a post
  */
-router.post("/", (req, res) => {
-	res.status(200).json({ message: "Add a post" });
-});
+router.post("/", addPost);
 
 /**
  *
  * PUT - Update a Post
  */
-router.put("/:id", (req, res) => {
-	res.status(200).json({ message: `Update Post with id : ${req.params.id}` });
-});
+router.put("/:id", updatePost);
 
 /**
  *
  * DELETE - Delete Post by id
  */
-router.delete("/:id", (req, res) => {
-	res.status(200).json({ message: `Delete Post with id : ${req.params.id}` });
-});
+router.delete("/:id", deletePost);
 
 module.exports = router;
