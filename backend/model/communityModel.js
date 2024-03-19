3fb0b8799745f6500f0d57c4742eb30a2e4c5c60
Mohamad-Kazem Abd-el-Sater <mkhas313@gmail.com>
2024-03-19 10:54:59 +0200

    Add model folder, Define communityModel.js

---
+++ backend/model/communityModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const { Schema, model, SchemaTypes } = mongoose;
+
+const communitySchema = new Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		description: String,
+		members: [SchemaTypes.ObjectId],
+		communityAdmins: [SchemaTypes.ObjectId],
+		communityMods: [SchemaTypes.ObjectId],
+		isPrivate: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	{ timestamps: true }
+);
+
+const Community = model("Community", communitySchema);
+export default Community;
